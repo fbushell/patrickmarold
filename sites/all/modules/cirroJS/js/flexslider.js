cde50acfcb8cbff6c46cea7b69491f8e2c3fb28f
fbushell <frank@gocirro.com>
2012-10-09 22:14:28 -0600

    killed flexslider idea

--- sites/all/modules/cirroJS/js/flexslider.js
+++ sites/all/modules/cirroJS/js/flexslider.js
@@ -4,10 +4,6 @@ jQuery(document).ready(function($){
   $('.flexslider').flexslider({
     animation: "slide",
     animationLoop: true,
-    itemWidth: 900,
-    itemMargin: 20,
-    minItems: 4,
-    maxItems: 6
   });
 
   
