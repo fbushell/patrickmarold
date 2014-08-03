7174912d05381b06f0f458c3d12cdb0acd113b75
fbushell <frank@gocirro.com>
2014-08-03 15:37:00 -0600

    added video link to blog node page

--- sites/all/themes/climBootstrap/templates/node--blog.tpl.php
+++ sites/all/themes/climBootstrap/templates/node--blog.tpl.php
@@ -4,10 +4,30 @@
     <?php 
     	$created = $node->created;
     	$date = date( "F j, Y", $created );
+      $video = '';
     ?>
     
     <h4><?php print $title; ?></h4>
-    <?php print $date; ?>
+    <div class="blog-date"><?php print $date; ?></div>
+    <?php  
+    
+      if ( isset($node->field_blog_video_link['und'][0]['value']) ) {
+        $video_link = $node->field_blog_video_link['und'][0]['value'];
+
+        $video = "
+          <iframe src=\"http://player.vimeo.com/video/{$video_link}?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff\" 
+            width=\"872\" 
+            height=\"490\" 
+            frameborder=\"0\" 
+            webkitAllowFullScreen mozallowfullscreen allowFullScreen>
+          </iframe>
+        ";
+      }
+
+    ?>
+
+    <?php print $video; ?>
+
   	<div class="content">
   	  <?php print render($content); ?>
   	</div>
